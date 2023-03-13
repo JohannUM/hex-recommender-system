9674cc76248a614d0b7ad75e3d220c1cbff2fca3
Johann <72266031+JohannUM@users.noreply.github.com>
2023-03-13 15:35:57 +0100

    initial push

---
+++ hex.py
@@ -0,0 +1,26 @@
+class Hex:
+
+    def __init__(self, gridsize:tuple):
+        self.gridsize = gridsize
+        
+        pass
+
+    def init_board(self):
+        board = []
+        return board
+    
+    def make(self):
+        self.board = self.init_board()
+        pass
+
+    def step(self, action):
+        pass
+
+    def reset(self):
+        pass
+
+    
+        
+
+
+    
