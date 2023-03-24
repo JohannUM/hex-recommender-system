396430a6e7f224cf508760c598ec0e5753e6f67d
MatthewFoster02 <mattfoster02@outlook.com>
2023-03-24 12:09:17 +0100

    Update agent.py

--- src/agent/agent.py
+++ src/agent/agent.py
@@ -1,3 +1,5 @@
+from copy import deepcopy
+
 from game.board import Board
 from agent import minimax
 from agent.mcts import MCTS
@@ -15,18 +17,37 @@ class Agent:
         }
 
 
-    def find_move(self, player, board:Board, hybrid_threshold=0.7):
+    def find_move(self, player, board:Board):
+        move = None
         if self.type == 'minimax':
-            return minimax.find_move(board, player)
+            move = minimax.find_move(board, player)
         elif self.type == 'mcts':
             mcts = MCTS(player=player, game_state=board, max_depth=5)
-            return mcts.predict()
+            move = mcts.predict()
         elif self.type == 'hybrid':
-            if board.percentage_occupied() >= self.hybrid_tresholds[board.gridsize]:
-                print('minimax')
-                return minimax.find_move(board, player)
+            if board.percentage_occupied() > self.hybrid_tresholds[board.gridsize]:
+                #print('minimax')
+                move = minimax.find_move(board, player)
             else:
-                print('mcts')
+                #print('mcts')
                 mcts = MCTS(player=player, game_state=board, max_depth=5)
-                return mcts.predict()
+                move = mcts.predict()
+        self.explainMove(move, board)
+        return move
+    
+    def explainMove(self, player:int, move:tuple, board:Board):
+        board_copy = deepcopy(board)
+        board_copy.update_position_state(move, player)
+        if board_copy.check_winner() == player:
+            print(f'This move wins the game for player {player}.')
+            return
+        
+        board_copy.update_position_state(move, 3-player)
+        if board_copy.check_winner() == 3-player:
+            print(f'This move blocks the opposing player from winning.')
+            return
+        
+        print(f'This move advances the game and increases player {player}s chance of winning.')
+        
+
             
